a4cebabbd5896b72ddb91dbcddb6531b37fda125
jackson273 <63116993+jackson273@users.noreply.github.com>
2022-03-31 13:25:35 +0300

    return init

--- app.py
+++ app.py
@@ -17,44 +17,6 @@ IMAGES_ROOT = config.IMAGES_ROOT
 
 # ----------------------------Routes------------------------------------ #
 
-@app.route('/index/', methods=['GET'])
-def profile():
-    username = request.cookies.get('username', '')
-    user_query = User.filter(username=username)
-    if user_query:
-        user = user_query[0]
-    else:
-        user = None
-    posts = User.get_all_posts_of_user()
-    return render_template('index.html', user=user, posts=posts)
-
-@app.route('/', methods=['GET'])
-def index():
-    return redirect(url_for('profile'))
-
-@app.route('/create-post/', methods=['POST'])
-@login_required
-def create_post():
-    title = request.form.get('title', '')
-    text = request.form.get('text', '')
-    username = request.cookies.get('username', '')
-    user_query = User.filter(username=username)
-    if 'image' in request.files: 
-        image = request.files['image']
-        if image and allowed_file(image.filename):
-            if user_query:
-                user = user_query[0]
-                filename = secure_filename(image.filename)
-                imageURL = os.path.join(config.IMAGES_ROOT, filename)
-                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                user.create_post(title, text, imageURL)
-                user.save()
-                flash('Created successfully!!!', 'message')
-                return redirect(url_for('profile'))
-
-    flash('Error when trying create your post!!!', 'error')
-    return redirect(url_for('profile'))
-
 @app.route('/profile/', methods=['GET'])
 @login_required
 def profile():
